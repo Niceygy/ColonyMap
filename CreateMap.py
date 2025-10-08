29d55699542a82a023c1bff3f7ee34ed4d28b161
Niceygy <niceygy@niceygy.net>
2025-10-08 20:00:41 +0100

    3D Working

--- CreateMap.py
+++ CreateMap.py
@@ -2,6 +2,7 @@ import json
 import numpy
 import math
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D  # Add this import
 
 class ScatterPlotCreator:
     def process_to_array(file_name: str):
@@ -17,12 +18,13 @@ class ScatterPlotCreator:
                 result.append([
                     item['coords']['x'],
                     item['coords']['y'],
+                    item['coords']['z'],
                     item['population']
                 ])
                 
                 percent = round((current_item / length) * 100)
                 print(
-                    f"\rProcessing to numpy array... ({current_item} / {length}, {percent}%)",
+                    f"\rProcessing to array... ({current_item} / {length}, {percent}%)",
                     end='', flush=True
                     )
                 current_item += 1            
@@ -32,15 +34,18 @@ class ScatterPlotCreator:
             return result
         
     def create_plot(array):
-        print("Creating plot...")
+        print("\nCreating plot...")
         x = array[:, 0]
         y = array[:, 1]
-        plt.scatter(x, y, s=1)
-        plt.xlabel("X")
-        plt.ylabel("Y")
-        plt.title("System scatter plot")
-        plt.savefig('plot_image.png', dpi=300)
-        print("Plot saved as plot_image.png")
+        z = array[:, 2]
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(x, y, z, s=1)
+        ax.set_xlabel("X")
+        ax.set_ylabel("Y")
+        ax.set_zlabel("Z")
+        ax.set_title("System 3D scatter plot")
+        plt.show()
             
             
 if __name__ == "__main__":
